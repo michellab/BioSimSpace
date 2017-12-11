d5ba28c83125dc1b6d5d02a81a13f7ff7037b3ff
Lester Hedges <lester.hedges@gmail.com>
2017-12-11 09:28:22 +0000

    Removed redundant code.

--- python/BioSimSpace/Sample/__init__.py
+++ python/BioSimSpace/Sample/__init__.py
@@ -1,9 +1,3 @@
 import Sire.Base
 
 from .namd_process import NamdProcess
-
-#namd = Sire.Base.findExe('namd2')
-
-#proc = NamdProcess(1, 2)
-#print(proc.a, proc.b)
-#proc.run(namd.absoluteFilePath())
