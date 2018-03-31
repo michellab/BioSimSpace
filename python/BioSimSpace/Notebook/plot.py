a292c062ae5ea71c29ceb406d05fac0e64d9468a
Lester Hedges <lester.hedges@gmail.com>
2018-03-31 20:17:47 +0100

    Only set pyplot properties if matplotlib import succeeded. [closes #7]

--- python/BioSimSpace/Notebook/plot.py
+++ python/BioSimSpace/Notebook/plot.py
@@ -11,19 +11,20 @@ try:
 except ImportError:
     _has_matplotlib = False
 
-# Define font sizes.
-SMALL_SIZE = 14
-MEDIUM_SIZE = 16
-BIGGER_SIZE = 18
+if _has_matplotlib:
+    # Define font sizes.
+    SMALL_SIZE = 14
+    MEDIUM_SIZE = 16
+    BIGGER_SIZE = 18
 
-# Set font sizes.
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    # Set font sizes.
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 def Plot(x=None, y=None, xlabel=None, ylabel=None):
     """A simple function to create x/y plots with matplotlib."""
