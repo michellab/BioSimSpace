c56798984669e03a789db55e088f2bfd6e47080c
Lester Hedges <lester.hedges@gmail.com>
2018-06-12 16:25:02 +0100

    First attempt at switching molecule compatibility. [ci skip]

--- python/BioSimSpace/_SireWrappers/_molecule.py
+++ python/BioSimSpace/_SireWrappers/_molecule.py
@@ -27,8 +27,12 @@ Author: Lester Hedges <lester.hedges@gmail.com>
 """
 
 import Sire.Mol as _SireMol
+import Sire.MM as _SireMM
 
 __all__ = ["Molecule"]
+
+class IncompatibleError(Exception):
+    pass
 
 class Molecule():
     """A container class for storing a molecule."""
@@ -67,3 +71,99 @@ class Molecule():
     def _getSireMolecule(self):
         """Return the full Sire Molecule object."""
         return self._molecule
+
+    def _makeCompatibleWith(self, molecule, overwrite=False):
+        """Make this molecule compatible with this one, i.e. match atoms and
+           add all additional properties.
+
+           Positional arguments:
+
+           molecule  -- The molecule to match with.
+
+           Keyword arguments:
+
+           overwrite -- Whether to overwrite any duplicate properties.
+        """
+
+        # Check that the molecule is valid.
+        if isinstance(molecule, _SireMol.Molecule):
+            mol1 = molecule
+        if type(molecule) is Molecule:
+            mol1 = molecule._molecule
+        else:
+            raise TypeError("'molecule' must be of type 'BioSimSpace.Molecule', or 'Sire.Mol._Mol.Molecule'")
+
+        # Get the two Sire molecules.
+        mol0 = self._molecule
+
+        # Store the number of atoms to match.
+        num_atoms = mol0.nAtoms()
+
+        # The new molecule must have at least as many atoms.
+        if mol1.nAtoms() < num_atoms:
+            raise IncompatibleError("The passed molecule does not contain enough atoms!")
+
+        # Match the atoms based on residue index and atom name.
+        matches = _SireMol.ResIdxAtomNameMatcher().match(mol0, mol1)
+
+        # Have we matched all of the atoms?
+        if len(matches) < num_atoms:
+            # Atom names might have changed. Try to match using residue index
+            # and maximum common substructure (matching light atoms too).
+            matches = _SireMol.ResIdxAtomMCS(True, False).match(mol0, mol1)
+
+            # Have we matched all of the atoms?
+            if len(matches) < num_atoms:
+                raise IncompatibleError("Failed to match all atoms!")
+
+        # Get a list of the property keys for each molecule.
+        props0 = mol0.propertyKeys()
+        props1 = mol1.propertyKeys()
+
+        # Create a dictionary to flag whether a property has been seen.
+        seen_prop = {}
+        for prop in props1:
+            seen_prop[prop] = False
+
+        # Make the molecule editable.
+        edit_mol = mol0.edit()
+
+        # First set atom based properties.
+
+        # Loop over all of the keys in the new molecule.
+        for prop in props1:
+            # This is a new property, or we are allowed to overwrite.
+            if (not mol0.hasProperty(prop)) or overwrite:
+                # Loop over all of the atom mapping pairs and set the property.
+                for idx0, idx1 in matches.items():
+                    # Does the atom have this property?
+                    # If so, add it to the matching atom in this molecule.
+                    if mol1.atom(idx1).hasProperty(prop):
+                        try:
+                            edit_mol = edit_mol.atom(idx0).setProperty(prop, mol1.atom(idx1).property(prop)).molecule()
+                            seen_prop[prop] = True
+                        except:
+                            raise IncompatibleError("Failed to map property '%s' from AtomIdx(%d) to AtomIdx(%d)."
+                                % (prop, idx1, idx0))
+
+        # Now deal with all unseen properties. These will be non atom-based
+        # properties, such as TwoAtomFunctions, StringProperty, etc.
+        for prop in seen_prop:
+            # Skip parameters and intrascale, since these contain an implicit
+            # reference to a molinfo object.
+            if not seen_prop[prop] and prop != "parameters":
+                # This is a new property, or we are allowed to overwrite.
+                if (not mol0.hasProperty(prop)) or overwrite:
+                    # Try to match with atoms in the original molecule.
+                    try:
+                        edit_mol.setProperty(prop, mol1.property(prop).makeCompatibleWith(mol0, matches))
+
+                    # This is probably just a molecule property, such as a forcefield definition.
+                    except AttributeError:
+                        try:
+                            edit_mol.setProperty(prop, mol1.property(prop))
+                        except:
+                            raise IncompatibleError("Failed to set property '%s'" % prop)
+
+        # Commit the changes.
+        self._molecule = edit_mol.commit()
