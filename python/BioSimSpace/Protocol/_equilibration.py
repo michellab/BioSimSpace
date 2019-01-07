eb8da7ff2cbbf85133430aa9db55e8d59a11f78f
Lester Hedges <lester.hedges@gmail.com>
2019-01-07 11:53:01 +0000

    Updated copyright range. [ci skip]

--- python/BioSimSpace/Protocol/_equilibration.py
+++ python/BioSimSpace/Protocol/_equilibration.py
@@ -1,7 +1,7 @@
 ######################################################################
 # BioSimSpace: Making biomolecular simulation a breeze!
 #
-# Copyright: 2017-2018
+# Copyright: 2017-2019
 #
 # Authors: Lester Hedges <lester.hedges@gmail.com>
 #
