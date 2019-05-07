fea4ad35ad7aaa8ce0f083569ecce7a2469b67c4
Lester Hedges <lester.hedges@gmail.com>
2019-05-07 10:49:32 +0100

    Improved functionality for MD process wrapper.

--- python/BioSimSpace/MD/_md.py
+++ python/BioSimSpace/MD/_md.py
@@ -7,8 +7,7 @@
 #
 # BioSimSpace is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, either version 2 of the License, or # (at your option) any later version.
 #
 # BioSimSpace is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,15 +24,14 @@ Functionality for configuring and driving molecular dynamics simulations.
 
 import os as _os
 
-from Sire.Base import findExe as _findExe
+import Sire.Base as _SireBase
 
-from BioSimSpace import _amber_home
+from BioSimSpace import _amber_home, _gmx_exe
 from .._Exceptions import MissingSoftwareError as _MissingSoftwareError
-from ..Protocol import Custom as _Custom
-from ..Protocol._protocol import Protocol as _Protocol
 from .._SireWrappers import System as _System
 
 import BioSimSpace.Process as _Process
+import BioSimSpace.Protocol as _Protocol
 
 __author__ = "Lester Hedges"
 __email_ = "lester.hedges@gmail.com"
@@ -41,20 +39,37 @@ __email_ = "lester.hedges@gmail.com"
 __all__ = ["run"]
 
 # A dictionary mapping MD packages to their executable names and GPU support.
-#                PACKAGE        EXE           GPU
-_md_packages = { "AMBER"   : { "pmemd.cuda" : True, "pmemd" : False, "sander" : False },
-                 "GROMACS" : { "gmx" : True },
-                 "NAMD"    : { "namd2" : False }
+#                PACKAGE        EXE               GPU
+_md_packages = { "AMBER"   : { "pmemd.cuda.MPI" : True,
+                               "pmemd.cuda"     : True,
+                               "pmemd.MPI"      : False,
+                               "sander"         : False },
+                 "GROMACS" : { "gmx"            : True,
+                               "gmx_mpi"        : True },
+                 "SOMD"    : { "somd"           : True },
+                 "NAMD"    : { "namd2"          : False }
                }
 
-# A dictionary reverse mapping MD packages to their Sire file extensions.
-#                    EXTENSION     EXTENSION
-_file_extensions = { "PRM7,RST7"    : "AMBER",
-                     "PRM7,RST"     : "AMBER",
-                     "GroTop,Gro87" : "GROMACS",
-                     "PSF,PDB"      : "NAMD" }
+# A dictionary reverse mapping MD packages to their supported Sire file extensions.
+# Use SOMD as a fall-back where possible. Since we can't guarantee interconversion
+# of potentials for CHARMM-PSF format input files, we restrict such simulations to
+# only run using NAMD.
+#                    EXTENSION        PACKAGES
+_file_extensions = { "PRM7,RST7"    : ["AMBER", "GROMACS", "SOMD"],
+                     "PRM7,RST"     : ["AMBER", "GROMACS", "SOMD"],
+                     "GroTop,Gro87" : ["GROMACS", "AMBER", "SOMD"],
+                     "PSF,PDB"      : ["NAMD"]
+                   }
 
-def _find_md_package(system, protocol, use_gpu=True):
+# Whether each package supports free energy simulations. This dictionary needs to
+# be updated as support for different packages is added.
+_free_energy = { "AMBER"   : False,
+                 "GROMACS" : True,
+                 "SOMD"    : True,
+                 "NAMD"    : False
+                }
+
+def _find_md_package(system, protocol, gpu_support=False):
     """Find a molecular dynamics package on the system and return
        a handle to it as a MDPackage object.
 
@@ -67,23 +82,18 @@ def _find_md_package(system, protocol, use_gpu=True):
        protocol : :class:`Protocol <BioSimSpace.Protocol>`
            The simulation protocol.
 
-       use_gpu : bool
-           Whether to use GPU support.
+       gpu_support : bool
+           Whether to use package must have GPU support.
 
        Returns
        -------
 
-       (package, exe) : tuple
+       (package, exe) : (str, str)
            The name of the MD package and a path to its executable.
     """
 
-    # Check that the system is valid.
-    if type(system) is not _System:
-        raise TypeError("'system' must be of type 'BioSimSpace._SireWrappers.System'")
-
-    # Check that the use_gpu flag is valid.
-    if type(use_gpu) is not bool:
-        raise TypeError("'use_gpu' keyword must be of type 'bool'")
+    # The input has already been validated in the run method, so no need
+    # to re-validate here.
 
     # Get the file format of the molecular system.
     fileformat = system.fileFormat()
@@ -92,29 +102,57 @@ def _find_md_package(system, protocol, use_gpu=True):
     if not fileformat in _file_extensions:
         raise ValueError("Cannot find an MD package that supports format: %s" % fileformat)
     else:
-        package = _file_extensions[fileformat]
+        packages = _file_extensions[fileformat]
 
-    # Loop over each executable in turn and see whether it exists on the system.
-    for exe, gpu in _md_packages[package].items():
-        if package == "AMBER":
-            # Search AMBERHOME, if set.
-            if _amber_home is not None:
-                _exe = "%s/bin/%s" % (_amber_home, exe)
-                if _os.path.isfile(_exe):
-                    return (package, _exe)
+    # Is this a free energy protocol.
+    if type(protocol) is _Protocol.FreeEnergy:
+        is_free_energy = True
+    else:
+        is_free_energy = False
 
-        # Search system PATH.
-        else:
-            try:
-                exe = _findExe(exe).absoluteFilePath()
-                return (package, exe)
-            except:
-                pass
+    # Loop over each package that supports the file format.
+    for package in packages:
+        # If this is free energy protocol, then check that the package has support.
+        if not is_free_energy or _free_energy[package]:
+            # Check whether this package exists on the system and has the desired
+            # GPU support.
+            for exe, gpu in _md_packages[package].items():
+                # If the user has requested GPU support make sure the package
+                # supports it.
+                if not gpu_support or gpu:
+                    # AMBER
+                    if package == "AMBER":
+                        # Search AMBERHOME, if set.
+                        if _amber_home is not None:
+                            _exe = "%s/bin/%s" % (_amber_home, exe)
+                            if _os.path.isfile(_exe):
+                                return (package, _exe)
+                        # Search system PATH.
+                        else:
+                            try:
+                                exe = _SireBase.findExe(exe).absoluteFilePath()
+                                return (package, exe)
+                            except:
+                                pass
+                    # GROMACS
+                    elif package == "GROMACS":
+                        if _gmx_exe is not None:
+                            return (package, _gmx_exe)
+                    # SOMD
+                    elif package == "SOMD":
+                        return (package, _SireBase.getBinDir() + "/somd")
+                    # Search system PATH.
+                    else:
+                        try:
+                            exe = _SireBase.findExe(exe).absoluteFilePath()
+                            return (package, exe)
+                        except:
+                            pass
 
-    # If we get this far, then no executable was found.
-    raise _MissingSoftwareError("No executable found for package: '%s'" % package) from None
+    # If we get this far, then no package was found.
+    raise _MissingSoftwareError("Couldn't find package to support format: %s" % fileformat)
 
-def run(system, protocol, autostart=True,
+def run(system, protocol, gpu_support=False, autostart=True,
         name="md", work_dir=None, seed=None, property_map={}):
     """Auto-configure and run a molecular dynamics process.
 
@@ -126,6 +164,9 @@ def run(system, protocol, autostart=True,
 
        protocol : :class:`Protocol <BioSimSpace.Protocol>`
            The simulation protocol.
+
+       gpu_support : bool
+           Whether to choose a package with GPU support.
 
        autostart : bool
            Whether to start the process automatically.
@@ -156,29 +197,63 @@ def run(system, protocol, autostart=True,
         raise TypeError("'system' must be of type 'BioSimSpace._SireWrappers.System'")
 
     # Check that the protocol is valid.
-    if not isinstance(protocol, _Protocol):
+    if not isinstance(protocol, _Protocol._protocol.Protocol):
         if type(protocol) is str:
-            protocol = _Custom(protocol)
+            protocol = _Protocol.Custom(protocol)
         else:
             raise TypeError("'protocol' must be of type 'BioSimSpace.Protocol' "
                             "or the path to a custom configuration file.")
 
+    # Validate optional arguments.
+
+    if type(gpu_support) is not bool:
+        raise TypeError("'gpu_support' must be of type 'bool'")
+
+    if type(autostart) is not bool:
+        raise TypeError("'autostart' must be of type 'bool'")
+
+    if type(name) is not str:
+        raise TypeError("'name' must be of type 'str'")
+
+    if work_dir is not None:
+        if type(work_dir) is not str:
+            raise TypeError("'work_dir' must be of type 'str'")
+
+    if seed is not None:
+        if type(seed) is not int:
+            raise TypeError("'seed' must be of type 'int'")
+
+    if type(property_map) is not dict:
+        raise TypeError("'property_map' must be of type 'dict'")
+
     # Find a molecular dynamics package and executable.
-    package, exe = _find_md_package(system, protocol)
+    package, exe = _find_md_package(system, protocol, gpu_support)
 
     # Create the process object.
 
     # AMBER.
     if package == "AMBER":
-        process = _Process.Amber(system, protocol, exe, name, work_dir, seed, property_map)
+        process = _Process.Amber(system, protocol, exe=exe, name=name,
+            work_dir=work_dir, seed=seed, property_map=property_map)
 
     # GROMACS.
     elif package == "GROMACS":
-        process = _Process.Gromacs(system, protocol, exe, name, work_dir, seed, property_map)
+        process = _Process.Gromacs(system, protocol, exe=exe, name=name,
+            work_dir=work_dir, seed=seed, property_map=property_map)
+
+    # SOMD.
+    elif package == "SOMD":
+        if gpu_support:
+            platform = "CUDA"
+        else:
+            platform = "CPU"
+        process = _Process.Somd(system, protocol, exe=exe, name=name,
+            work_dir=work_dir, seed=seed, property_map=property_map, platform=platform)
 
     # NAMD.
     elif package == "NAMD":
-        process = _Process.Namd(system, protocol, exe, name, work_dir, seed, property_map)
+        process = _Process.Namd(system, protocol, exe=exe, name=name,
+            work_dir=work_dir, seed=seed, property_map=property_map)
 
     # Start the process.
     if autostart:
--- test/MD/test_md.py
+++ test/MD/test_md.py
@@ -15,6 +15,12 @@ if BSS._amber_home is not None:
 else:
     has_amber = False
 
+# Make sure GROMACS is installed.
+if BSS._gmx_exe is None:
+    has_gromacs = False
+else:
+    has_gromacs = True
+
 # Make sure NAMD is installed.
 try:
     findExe("namd2")
@@ -24,7 +30,7 @@ except:
 
 @pytest.mark.skipif(has_amber is False, reason="Requires AMBER to be installed.")
 def test_amber():
-    """Test a short AMBER minimisation protocol with MD driver."""
+    """Test a short AMBER minimisation protocol with the MD driver."""
 
     # Create a short minimisation protocol.
     protocol = BSS.Protocol.Minimisation(steps=100)
@@ -44,15 +50,37 @@ def test_amber():
     # Check that the process finishes without error.
     assert not process.isError()
 
-@pytest.mark.skipif(has_namd is False, reason="Requires NAMD to be installed.")
-def test_namd():
-    """Test a short NAMD minimisation protocol with MD driver."""
+@pytest.mark.skipif(has_gromacs is False, reason="Requires GROMACS to be installed.")
+def test_gromacs():
+    """Test a short GROMACS minimisation protocol with the MD driver."""
 
     # Create a short minimisation protocol.
     protocol = BSS.Protocol.Minimisation(steps=100)
 
     # Glob the input files.
-    files = BSS.IO.glob("test/io/amber/ala/*")
+    files = BSS.IO.glob("test/io/gromacs/kigaki/*")
+
+    # Load the molecular system.
+    system = BSS.IO.readMolecules(files)
+
+    # Initialise the GROMACS process.
+    process = BSS.MD.run(system, protocol, name="test")
+
+    # Wait for the process to end.
+    process.wait()
+
+    # Check that the process finishes without error.
+    assert not process.isError()
+
+@pytest.mark.skipif(has_namd is False, reason="Requires NAMD to be installed.")
+def test_namd():
+    """Test a short NAMD minimisation protocol with the MD driver."""
+
+    # Create a short minimisation protocol.
+    protocol = BSS.Protocol.Minimisation(steps=100)
+
+    # Glob the input files.
+    files = BSS.IO.glob("test/io/namd/alanin/*")
 
     # Load the molecular system.
     system = BSS.IO.readMolecules(files)
