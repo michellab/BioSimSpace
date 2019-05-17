d74da22721087a698a2f43da8237ea5793d74c54
Lester Hedges <lester.hedges@gmail.com>
2019-05-17 14:28:08 +0100

    Updated example notebooks and scripts.

--- demo/conversion.py
+++ demo/conversion.py
@@ -4,9 +4,9 @@
 # Author: Lester Hedges<br>
 # Email:&nbsp;&nbsp; lester.hedges@bristol.ac.uk
 # 
-# # Minimisation
+# # Conversion
 # 
-# In this notebook you'll learn how to use BioSimSpace to write a robust and interoperable workflow node to perform energy minimisation on a molecular system.
+# In this notebook you'll learn how to use BioSimSpace to convert between molecular file formats.
 # 
 # First we'll need to import BioSimSpace:
 
@@ -21,7 +21,7 @@ import BioSimSpace as BSS
 # In[ ]:
 
 
-node = BSS.Gateway.Node("A node to perform energy minimisation and save the minimised molecular configuration to file.")
+node = BSS.Gateway.Node("A node to convert between molecular file formats.")
 
 
 # We'll now set the author and license:
@@ -33,28 +33,28 @@ node.addAuthor(name="Lester Hedges", email="lester.hedges@bristol.ac.uk", affili
 node.setLicense("GPLv3")
 
 
-# Nodes require inputs. To specify inputs we use the `Gateway` module, which is used as a bridge between BioSimSpace and the outside world. This will allow us to document the inputs, define their type, and specify any constraints on their allowed values. Here we will need a set of files that define the molecular system, and an integer that indicates the number of minimisation steps to perform.
+# Nodes require inputs. To specify inputs we use the `Gateway` module, which is used as a bridge between BioSimSpace and the outside world. This will allow us to document the inputs, define their type, and specify any constraints on their allowed values. Here we will need a set of files that define the molecular system, and a string that will specify the format to convert to. Note that the string can only be one of a set of allowed values. We directly query the `BSS.IO` package to generate the dropdown of allowed options. (If you don't know what these formats are, then run, e.g. `BSS.IO.formatInfo("GroTop")` for a description.)
 
 # In[ ]:
 
 
 node.addInput("files", BSS.Gateway.FileSet(help="A set of molecular input files."))
-node.addInput("steps", BSS.Gateway.Integer(help="The number of minimisation steps.", minimum=0, maximum=1000000, default=10000))
+node.addInput("format", BSS.Gateway.String(help="The molecular file format to convert to.",
+                                           allowed=BSS.IO.fileFormats(),
+                                           default="PDB"))
 
 
-# Note that the input requirement `steps` has a default value, so it is optional.
-# 
-# We now need to define the output of the node. In this case we will return a set of files representing the minimised molecular system.
+# We now need to define the output of the node. In this case we will return a file containing the converted molecular system.
 
 # In[ ]:
 
 
-node.addOutput("minimised", BSS.Gateway.FileSet(help="The minimised molecular system."))
+node.addOutput("converted", BSS.Gateway.File(help="The converted molecular system."))
 
 
 # When working interactively within a Jupyter notebook we need a way to allow users to set the input requirements. The `node.showControls` method will display a graphical user interface (GUI), from which inputs can be set.
 # 
-# Note that the GUI requires active user input. All input requirements that don't have a default value _must_ be set before the node can proceed. If you try to query the node for one of the user values then an error will be raised. For bounded integer inputs you can use a slider to set the value, or type in the input box and press enter.
+# Note that the GUI requires active user input. All input requirements that don't have a default value _must_ be set before the node can proceed. If you try to query the node for one of the user values then an error will be raised. Use the dropdown button to choose a file format from the list of allowed values.
 # 
 # When working interactively you will typically be running on a remote server where you won't have access to the local filesystem. In this case you'll need to upload files for any of the `File` or `FileSet` input requirements. The GUI below will provide buttons that allow you to browse your own filesystem and select files. Since Jupyter has a limit of 5MB for file transfers, we provide support for compressed formats, such as `.zip` or `.tar.gz`. (A single archive can contain a set of files, allowing you to set a single value for a `FileSet` requirement.) We've provided some example input files that can be used in the training notebooks, which are available to download from the links below. These can then be re-uploaded using the GUI.
 # 
@@ -63,7 +63,6 @@ node.addOutput("minimised", BSS.Gateway.FileSet(help="The minimised molecular sy
 # GROMACS: [kigaki.gro](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/gromacs/kigaki/kigaki.gro), [kigaki.top](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/gromacs/kigaki/kigaki.top)
 # 
 # NAMD: [alanin.pdb](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/namd/alanin/alanin.pdb), [alanin.psf](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/namd/alanin/alanin.psf), [alanin.params](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/namd/alanin/alanin.params)
-# 
 # 
 # When uploading files the name of the current file will replace the `Browse` button. If you need to change the file, simply click on the button again and choose a new file. For `FileSet` requirements, a new `Browse` button will appear whenever an additional file is uploaded.
 
@@ -83,30 +82,17 @@ node.showControls()
 system = BSS.IO.readMolecules(node.getInput("files"))
 
 
-# In order to run a minimisation we need to define a protocol. This can be done using the `BioSimSpace.Protocol` module. Here we will create a "best practice" minimisation protocol, overriding the number of steps with the input from the user.
+# Next we use the `BioSimSpace.IO` package to write the system to the chosen molecular file format and set this as the output variable of the node. If the format conversion is not possible then an error will be thrown.
 
 # In[ ]:
 
 
-protocol = BSS.Protocol.Minimisation(steps=node.getInput("steps"))
+# Get the user specified format.
+format = node.getInput("format")
 
-
-# We now have everything that is required to run a minimisation. To do so, we use `BioSimSpace.MD` to find an appropriate molecular dynamics package on our current environment. What package is found will depend upon both the system and protocol, as well as the hardware that is available to the user.
-
-# In[ ]:
-
-
-process = BSS.MD.run(system, protocol)
-
-
-# When the process has finished running we can get the minimised molecular configuration. We will save this to file using the same format as the original system, and set the output requirement to the list of file names that were written.
-# 
-# Note that we pass `block=True` to `getSystem`. Since we're working interactively the process is running in the background and `getSystem` will grab and return the _latest_ molecular configuration. By passing `block=True` we wait for the process to finish before getting the _final_ configuration and writing it to file. (You will see that an asterisk appeak next to the cell until the process completes.)
-
-# In[ ]:
-
-
-node.setOutput("minimised", BSS.IO.saveMolecules("minimised", process.getSystem(block=True), system.fileFormat()))
+# Note that the `saveMolecules` function returns a list containing the names of the files to which it wrote.
+# Since there is only one file, we take the first item from the list.
+node.setOutput("converted", BSS.IO.saveMolecules("converted", system, format)[0])
 
 
 # Finally, we validate that the node completed succesfully. This will check that all output requirements are satisfied and that no errors were raised by the user. Any file outputs will be available for the user to download as a compressed archive.
--- demo/input_files.py
+++ demo/input_files.py
@@ -4,9 +4,9 @@
 # Author: Lester Hedges<br>
 # Email:&nbsp;&nbsp; lester.hedges@bristol.ac.uk
 # 
-# # Minimisation
+# # Input files
 # 
-# In this notebook you'll learn how to use BioSimSpace to write a robust and interoperable workflow node to perform energy minimisation on a molecular system.
+# In this notebook you'll learn how to use BioSimSpace to generate input files for different molecular dynamics packages.
 # 
 # First we'll need to import BioSimSpace:
 
@@ -21,7 +21,7 @@ import BioSimSpace as BSS
 # In[ ]:
 
 
-node = BSS.Gateway.Node("A node to perform energy minimisation and save the minimised molecular configuration to file.")
+node = BSS.Gateway.Node("A node to create input files for molecular dynamics simulation.")
 
 
 # We'll now set the author and license:
@@ -33,28 +33,33 @@ node.addAuthor(name="Lester Hedges", email="lester.hedges@bristol.ac.uk", affili
 node.setLicense("GPLv3")
 
 
-# Nodes require inputs. To specify inputs we use the `Gateway` module, which is used as a bridge between BioSimSpace and the outside world. This will allow us to document the inputs, define their type, and specify any constraints on their allowed values. Here we will need a set of files that define the molecular system, and an integer that indicates the number of minimisation steps to perform.
+# Nodes require inputs. To specify inputs we use the `Gateway` module, which is used as a bridge between BioSimSpace and the outside world. This will allow us to document the inputs, define their type, and specify any constraints on their allowed values. Here we will need a set of files that define the molecular system, a string that will specify the type of simulation protocol that we'd like to perform, and another string to specify the molecular dynamics package. (Note that the strings take specific values, which can be automatically determined by querying BioSimSpace.)
 
 # In[ ]:
 
 
 node.addInput("files", BSS.Gateway.FileSet(help="A set of molecular input files."))
-node.addInput("steps", BSS.Gateway.Integer(help="The number of minimisation steps.", minimum=0, maximum=1000000, default=10000))
+node.addInput("protocol", BSS.Gateway.String(help="The molecular simulation protocol.",
+                                             allowed=BSS.Protocol.protocols(),
+                                             default="Minimisation"))
+node.addInput("package", BSS.Gateway.String(help="The molecular dynamics package.",
+                                            allowed=BSS.Process.packages(),
+                                            default="Amber"))
 
 
-# Note that the input requirement `steps` has a default value, so it is optional.
+# Note that we don't allow the user to specify any further details of the protocol. In this case we use a _best practice_ protocol.
 # 
-# We now need to define the output of the node. In this case we will return a set of files representing the minimised molecular system.
+# We now need to define the output of the node. In this case we will return the set of input files for the chosen molecular dynamics package.
 
 # In[ ]:
 
 
-node.addOutput("minimised", BSS.Gateway.FileSet(help="The minimised molecular system."))
+node.addOutput("input_files", BSS.Gateway.FileSet(help="A zip file containing the molecular dynamics input files."))
 
 
 # When working interactively within a Jupyter notebook we need a way to allow users to set the input requirements. The `node.showControls` method will display a graphical user interface (GUI), from which inputs can be set.
 # 
-# Note that the GUI requires active user input. All input requirements that don't have a default value _must_ be set before the node can proceed. If you try to query the node for one of the user values then an error will be raised. For bounded integer inputs you can use a slider to set the value, or type in the input box and press enter.
+# Note that the GUI requires active user input. All input requirements that don't have a default value _must_ be set before the node can proceed. If you try to query the node for one of the user values then an error will be raised. Use the dropdown button to choose options for the protocol and molecular dynamics package.
 # 
 # When working interactively you will typically be running on a remote server where you won't have access to the local filesystem. In this case you'll need to upload files for any of the `File` or `FileSet` input requirements. The GUI below will provide buttons that allow you to browse your own filesystem and select files. Since Jupyter has a limit of 5MB for file transfers, we provide support for compressed formats, such as `.zip` or `.tar.gz`. (A single archive can contain a set of files, allowing you to set a single value for a `FileSet` requirement.) We've provided some example input files that can be used in the training notebooks, which are available to download from the links below. These can then be re-uploaded using the GUI.
 # 
@@ -63,7 +68,6 @@ node.addOutput("minimised", BSS.Gateway.FileSet(help="The minimised molecular sy
 # GROMACS: [kigaki.gro](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/gromacs/kigaki/kigaki.gro), [kigaki.top](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/gromacs/kigaki/kigaki.top)
 # 
 # NAMD: [alanin.pdb](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/namd/alanin/alanin.pdb), [alanin.psf](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/namd/alanin/alanin.psf), [alanin.params](https://raw.githubusercontent.com/michellab/BioSimSpace/devel/demo/namd/alanin/alanin.params)
-# 
 # 
 # When uploading files the name of the current file will replace the `Browse` button. If you need to change the file, simply click on the button again and choose a new file. For `FileSet` requirements, a new `Browse` button will appear whenever an additional file is uploaded.
 
@@ -83,30 +87,22 @@ node.showControls()
 system = BSS.IO.readMolecules(node.getInput("files"))
 
 
-# In order to run a minimisation we need to define a protocol. This can be done using the `BioSimSpace.Protocol` module. Here we will create a "best practice" minimisation protocol, overriding the number of steps with the input from the user.
+# Next we setup a molecular dynamics process using the molecular system and the chosen simulation protocol. Note that an error will be thrown if the protocol isn't currently supported by the chosen molecular dynamics package.
 
 # In[ ]:
 
 
-protocol = BSS.Protocol.Minimisation(steps=node.getInput("steps"))
+process = BSS.Process.createProcess(system,
+                                    BSS.Protocol.createProtocol(node.getInput("protocol")),
+                                    node.getInput("package"))
 
 
-# We now have everything that is required to run a minimisation. To do so, we use `BioSimSpace.MD` to find an appropriate molecular dynamics package on our current environment. What package is found will depend upon both the system and protocol, as well as the hardware that is available to the user.
-
-# In[ ]:
-
-
-process = BSS.MD.run(system, protocol)
-
-
-# When the process has finished running we can get the minimised molecular configuration. We will save this to file using the same format as the original system, and set the output requirement to the list of file names that were written.
-# 
-# Note that we pass `block=True` to `getSystem`. Since we're working interactively the process is running in the background and `getSystem` will grab and return the _latest_ molecular configuration. By passing `block=True` we wait for the process to finish before getting the _final_ configuration and writing it to file. (You will see that an asterisk appeak next to the cell until the process completes.)
+# Now we extract a zip file containing the autogenerated input files from the process object and assign this to the output variable of the node.
 
 # In[ ]:
 
 
-node.setOutput("minimised", BSS.IO.saveMolecules("minimised", process.getSystem(block=True), system.fileFormat()))
+node.setOutput("input_files", process.getInput())
 
 
 # Finally, we validate that the node completed succesfully. This will check that all output requirements are satisfied and that no errors were raised by the user. Any file outputs will be available for the user to download as a compressed archive.
