6bcda532be936199badef0a01a7a11ee687cd087
Lester Hedges <lester.hedges@gmail.com>
2019-05-17 16:46:28 +0100

    Use build number to distinguish Conda package versions.

--- docker/conda-devel/update_recipe.sh
+++ docker/conda-devel/update_recipe.sh
@@ -18,10 +18,17 @@ TEMPLATE=$CONDA_DIR/template.yaml
 cp $TEMPLATE $RECIPE
 
 # Get the BioSimSpace version.
-BSS_VER=$(git --git-dir=$SRC_DIR/.git --work-tree=$SRC_DIR describe --tags | tr - _)
+BSS_VER=$(git --git-dir=$SRC_DIR/.git --work-tree=$SRC_DIR describe --tags --abbrev=0)
+
+# Get the build number. (Number of commits since last tag.)
+BSS_BUILD=$(git --git-dir=$SRC_DIR/.git --work-tree=$SRC_DIR log --oneline $BSS_VER.. | wc -l)
 
 # Update the BioSimSpace version number.
 echo "Updating BioSimSpace version number: '$BSS_VER'"
 sed -i.bak -e "s/VERSION/$BSS_VER/" $RECIPE && rm $RECIPE.bak
+
+# Update the build number.
+echo "Updating BioSimSpace build number: '$BSS_BUILD'"
+sed -i.bak -e "s/BUILD/$BSS_BUILD/" $RECIPE && rm $RECIPE.bak
 
 echo "Recipe updated!"
