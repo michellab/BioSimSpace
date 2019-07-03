4f1afd85c74537970b99531a35223887706f31cd
Lester Hedges <lester.hedges@gmail.com>
2019-07-03 16:23:23 +0100

    Added convenience function for initialising metadynamics processes.

--- python/BioSimSpace/Metadynamics/_metadynamics.py
+++ python/BioSimSpace/Metadynamics/_metadynamics.py
@@ -19,10 +19,90 @@
 #####################################################################
 
 """
-Functionality for configuring and driving metadynamics simulations.
+Functionality for initialising metadynamics simulation processes.
 """
 
 __author__ = "Lester Hedges"
 __email_ = "lester.hedges@gmail.com"
 
-#__all__ = ["run"]
+__all__ = ["run"]
+
+from .._SireWrappers import System as _System
+
+import BioSimSpace.Process as _Process
+import BioSimSpace.Protocol as _Protocol
+
+def run(system, protocol, auto_start=True, name="metamd", work_dir=None,
+     seed=None, property_map={}):
+    """Auto-configure and run a metadynamics process.
+
+       Parameters
+       ----------
+
+       system : :class:`System <BioSimSpace._SireWrappers.System>`
+           The molecular system.
+
+       protocol : :class:`Protocol <BioSimSpace.Protocol.Metadynamics>`
+           The metadynamics protocol.
+
+       auto_start : bool
+           Whether to start the process automatically.
+
+       name : str
+           The name of the process.
+
+       work_dir : str
+           The working directory for the process.
+
+       seed : int
+           A random number seed.
+
+       property_map : dict
+           A dictionary that maps system "properties" to their user defined
+           values. This allows the user to refer to properties with their
+           own naming scheme, e.g. { "charge" : "my-charge" }
+
+       Returns
+       -------
+
+       process : :class:`Process <BioSimSpace.Process>`
+           A process to run the molecular dynamics protocol.
+    """
+
+    # Check that the system is valid.
+    if type(system) is not _System:
+        raise TypeError("'system' must be of type 'BioSimSpace._SireWrappers.System'")
+
+    # Check that the protocol is valid.
+    if type(protocol) is not _Protocol.Metadynamics:
+        raise TypeError("'protocol' must be of type 'BioSimSpace.Protocol.Metadynamics'")
+
+    # Validate optional arguments.
+
+    if type(auto_start) is not bool:
+        raise TypeError("'auto_start' must be of type 'bool'")
+
+    if type(name) is not str:
+        raise TypeError("'name' must be of type 'str'")
+
+    if work_dir is not None:
+        if type(work_dir) is not str:
+            raise TypeError("'work_dir' must be of type 'str'")
+
+    if seed is not None:
+        if type(seed) is not int:
+            raise TypeError("'seed' must be of type 'int'")
+
+    if type(property_map) is not dict:
+        raise TypeError("'property_map' must be of type 'dict'")
+
+    # Create the process object.
+
+    process = _Process.Gromacs(system, protocol, name=name,
+        work_dir=work_dir, seed=seed, property_map=property_map)
+
+    # Start the process.
+    if auto_start:
+        return process.start()
+    else:
+        return process
