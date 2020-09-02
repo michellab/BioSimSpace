e67c85e6f2abc2207cf0640ef7c5153f40e66076
Lester Hedges <lester.hedges@gmail.com>
2020-09-02 12:10:37 +0100

    Added single point energy comparison in a triclinic cell.

--- test/Process/test_single_point_energy.py
+++ test/Process/test_single_point_energy.py
@@ -61,3 +61,54 @@ def test_amber_gromacs():
     nrg_amb = process_amb.getDihedralEnergy().kj_per_mol().magnitude()
     nrg_gmx = process_gmx.getDihedralEnergy().kj_per_mol().magnitude()
     assert nrg_amb == pytest.approx(nrg_gmx, rel=1e-2)
+
+@pytest.mark.skipif(has_amber is False or has_gromacs is False,
+    reason="Requires that both AMBER and GROMACS are installed.")
+def test_amber_gromacs_triclinic():
+    """Single point energy comparison between AMBER and GROMACS in a triclinic box."""
+
+    # Load the vacuum ubiquitin system.
+    files = BSS.IO.glob("test/io/amber/ubiquitin/*")
+    system = BSS.IO.readMolecules(files)
+
+    # Swap the space for a triclinic cell (truncated octahedron).
+    from Sire.Vol import TriclinicBox
+    triclinic_box = TriclinicBox.truncatedOctahedron(50)
+    system._sire_object.setProperty("space", triclinic_box)
+
+    # Create a single-step minimisation protocol.
+    protocol = BSS.Protocol.Minimisation(steps=1)
+
+    # Create a process to run with AMBER.
+    process_amb = BSS.Process.Amber(system, protocol)
+
+    # Create a process to run with GROMACS.
+    process_gmx = BSS.Process.Gromacs(system, protocol)
+
+    # Modify the GROMACS configuration to run zero steps.
+    config = process_gmx.getConfig()
+    config[1] = "nsteps = 0"
+    process_gmx.setConfig(config)
+
+    # Run the AMBER process and wait for it to finish.
+    process_amb.start()
+    process_amb.wait()
+
+    # Run the GROMACS process and wait for it to finish.
+    process_gmx.start()
+    process_gmx.wait()
+
+    # Compare bond energies. (In kJ / mol)
+    nrg_amb = process_amb.getBondEnergy().kj_per_mol().magnitude()
+    nrg_gmx = process_gmx.getBondEnergy().kj_per_mol().magnitude()
+    assert nrg_amb == pytest.approx(nrg_gmx, rel=1e-2)
+
+    # Compare angle energies. (In kJ / mol)
+    nrg_amb = process_amb.getAngleEnergy().kj_per_mol().magnitude()
+    nrg_gmx = process_gmx.getAngleEnergy().kj_per_mol().magnitude()
+    assert nrg_amb == pytest.approx(nrg_gmx, rel=1e-2)
+
+    # Compare dihedral energies. (In kJ / mol)
+    nrg_amb = process_amb.getDihedralEnergy().kj_per_mol().magnitude()
+    nrg_gmx = process_gmx.getDihedralEnergy().kj_per_mol().magnitude()
+    assert nrg_amb == pytest.approx(nrg_gmx, rel=1e-2)
