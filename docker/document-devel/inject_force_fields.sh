ac9e2b0420535ccc52c9db107c28b5c4f46a36cd
Lester Hedges <lester.hedges@gmail.com>
2021-01-21 09:05:24 +0000

    Inject force field names into __init__ file in source repository.

--- docker/document-devel/inject_force_fields.sh
+++ docker/document-devel/inject_force_fields.sh
@@ -10,5 +10,7 @@
 force_fields=$($HOME/sire.app/bin/python $HOME/BioSimSpace/docker/document-devel/supported_force_fields.py)
 
 # Inject the string into the __init__ of the BioSimSpace.Parameters package.
+# We use the source repository since the sire.app installation of BioSimSpace
+# is zipped, i.e. an egg file.
 # This is only run on Linux so the following, non-portable, sed command is fine.
-sed -i "s/__FORCE_FIELDS__/$forcefields/g" $HOME/sire.app/lib/python3.7/site-packages/BioSimSpace*/Parameters/__init__.py
+sed -i "s/__FORCE_FIELDS__/$forcefields/g" $HOME/BioSimSpace/python/BioSimSpace/Parameters/__init__.py
