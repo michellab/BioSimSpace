12dc32f8c7d21ebc6a642e7a407bf182abcaf17a
Lester Hedges <lester.hedges@gmail.com>
2021-04-17 22:28:14 +0100

    Fix docstring alignment.

--- python/BioSimSpace/Process/_openmm.py
+++ python/BioSimSpace/Process/_openmm.py
@@ -1045,11 +1045,11 @@ class OpenMM(_process.Process):
            index : int
                The index of the frame.
 
-          Returns
-          -------
+           Returns
+           -------
 
-          frame : :class:`System <BioSimSpace._SireWrappers.System>`
-              The System object of the corresponding frame.
+           frame : :class:`System <BioSimSpace._SireWrappers.System>`
+               The System object of the corresponding frame.
         """
 
         if type(index) is not int:
