48a936182cd9f071345cc46871b223e6101bbc4e
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2022-01-20 18:05:41 +0000

    Adding a test that verifies that the Sire Property wrapping is working correctly. This test fails on MacOS for older versions of Sire. It passes on Linux.

---
+++ test/_SireWrappers/test_properties.py
@@ -0,0 +1,48 @@
+
+import BioSimSpace as BSS
+
+def test_sire_properties():
+    s = BSS.IO.readMolecules("test/io/amber/ala/*")
+
+    m = s[0]._sire_object
+
+    charges = m.property("charge")
+
+    c = charges.toVector()
+
+    assert len(c) == m.nAtoms()
+
+    masses = m.property("mass")
+
+    v = masses.toVector()
+
+    assert len(v) == m.nAtoms()
+
+    a = m.property("ambertype")
+
+    a = a.toVector()
+
+    assert len(a) == m.nAtoms()
+
+    g = m.property("gb_radii")
+
+    g = g.toVector()
+
+    assert len(g) == m.nAtoms()
+
+    s = m.property("gb_screening")
+
+    s = s.toVector()
+
+    assert len(s) == m.nAtoms()
+
+    t = m.property("treechain")
+
+    t = t.toVector()
+
+    assert len(t) == m.nAtoms()
+
+
+if __name__ == "__main__":
+    test_sire_properties()
+
