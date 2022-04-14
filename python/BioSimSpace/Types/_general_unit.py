9e06e68484dd47026fbd6a010623be40781608d0
Lester Hedges <lester.hedges@gmail.com>
2022-04-14 11:48:14 +0100

    Typo.

--- python/BioSimSpace/Types/_general_unit.py
+++ python/BioSimSpace/Types/_general_unit.py
@@ -666,7 +666,7 @@ class GeneralUnit(_Type):
                 return GeneralUnit(general_unit)
 
             except:
-                raise ValueError(f"Could not infer GneralUnit from string '{string}'") from None
+                raise ValueError(f"Could not infer GeneralUnit from string '{string}'") from None
 
         else:
             raise TypeError("'string' must be of type 'str'")
