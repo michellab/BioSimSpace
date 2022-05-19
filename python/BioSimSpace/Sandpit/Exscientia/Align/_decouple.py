afb461b0ea1d0563c06946f34df0586552b65a2b
Zhiyi Wu <zwu@exscientia.ai>
2022-05-19 09:49:47 +0100

    Update _decouple.py

--- python/BioSimSpace/Sandpit/Exscientia/Align/_decouple.py
+++ python/BioSimSpace/Sandpit/Exscientia/Align/_decouple.py
@@ -1,3 +1,9 @@
+import warnings
+
+from .._SireWrappers import Molecule as _Molecule
+from .._Exceptions import IncompatibleError as _IncompatibleError
+
+
 def decouple(molecule, property_map0={}, property_map1={}, intramol=True):
     """Make the molecule as being decoupled, where the interactions with the
     rest of the environment are removed, or annihilate, where the interactions
@@ -23,12 +29,37 @@ def decouple(molecule, property_map0={}, property_map1={}, intramol=True):
             interactions e.g. { "charge" : False, "vdw" : False}
 
         intramol : bool
-            Whether to remove the intra-molecule forces, thereby transforming
-
+            Whether to remove the intra-molecule forces, thereby annihilate the
+            molecule instead of decouple it.
 
         Returns
         -------
 
-        merged : Sire.Mol.Molecule
-            The merged molecule.
+        decoupled : Sire.Mol.Molecule
+            The molecule marked as being decoupled.
     """
+    # Validate input.
+
+    if not isinstance(molecule, _Molecule):
+        raise TypeError("'molecule0' must be of type 'BioSimSpace._SireWrappers.Molecule'")
+
+    # Cannot decouple a perturbable molecule.
+    if molecule._is_perturbable:
+        raise _IncompatibleError("'molecule0' has already been merged!")
+
+    # Check the keys in the property_map
+    for property_map in (property_map0, property_map1):
+        if not isinstance(property_map, dict):
+            raise TypeError("'property_map' must be of type 'dict'")
+        for key in property_map:
+            if not key in ('charge', 'vdw'):
+                warnings.warn(f'Key {key} not supported for decouple, will be '
+                              f'ignored. The recognised keys are '
+                              f'("charge", "vdw")')
+
+    if not isinstance(intramol, bool):
+        raise TypeError("'intramol' must be of type 'bool'")
+
+    # Create a copy of this molecule.
+    mol = _Molecule(molecule)
+
