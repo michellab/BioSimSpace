7e105050de46101cd66164f3dfce6ec27b608af7
Zhiyi Wu <zwu@exscientia.ai>
2022-05-25 14:26:29 +0100

    update

--- test/Sandpit/Exscientia/Free_Energy/test_restraint.py
+++ test/Sandpit/Exscientia/Free_Energy/test_restraint.py
@@ -2,18 +2,14 @@ import pytest
 
 import BioSimSpace.Sandpit.Exscientia as BSS
 from BioSimSpace.Sandpit.Exscientia.Align._decouple import decouple
-from BioSimSpace.Sandpit.Exscientia.Protocol import Production
 from BioSimSpace.Sandpit.Exscientia.FreeEnergy import Restraint
-# from BioSimSpace.Sandpit.Exscientia import Process as _Process
 from BioSimSpace.Sandpit.Exscientia.Units.Length import angstrom
 from BioSimSpace.Sandpit.Exscientia.Units.Angle import radian
 from BioSimSpace.Sandpit.Exscientia.Units.Energy import kcal_per_mol
-from BioSimSpace.Sandpit.Exscientia._SireWrappers import Atom
-from BioSimSpace.Sandpit.Exscientia.Types import Length, Angle, Energy
 
 @pytest.fixture(scope='session')
 def restraint():
-    '''Generate the restaint object with _Boresch already filled.'''
+    '''Generate the Boresch restaint object.'''
     ligand = BSS.IO.readMolecules(BSS.IO.glob("test/input/ligands/ligand01*")).getMolecule(0)
     decoupled_ligand = decouple(ligand)
 
@@ -21,10 +17,6 @@ def restraint():
                                     'test/input/molecules/1jr5.top']).getMolecule(0)
 
     system = (protein + decoupled_ligand).toSystem()
-
-    protocol = Production()
-
-    restraint = Restraint(system, protocol=protocol, engine='GROMACS')
 
     # Assign three atoms from the ligand
     ligand_2 = decoupled_ligand.getAtoms()[2]
@@ -36,63 +28,75 @@ def restraint():
     protein_1 = protein.getAtoms()[1]
     protein_2 = protein.getAtoms()[2]
 
-    # To form the Boresch with arbitrary values
-    restraint._Boresch = {'Bonds': [
-        ((ligand_0, protein_0), 1 * angstrom, 2 * kcal_per_mol / angstrom ** 2),
-    ],
-        'Angles': [
-            ((ligand_1, ligand_0, protein_0), 3 * radian,
-             4 * kcal_per_mol / (radian * radian)),
-            ((ligand_0, protein_1, protein_2), 5 * radian,
-             6 * kcal_per_mol / (radian * radian)),
-        ],
-        'Dihedrals': [
-            ((ligand_2, ligand_1, ligand_0, protein_0), 7 * radian,
-             8 * kcal_per_mol / (radian * radian)),
-            ((ligand_1, ligand_0, protein_0, protein_1), 9 * radian,
-             10 * kcal_per_mol / (radian * radian)),
-            ((ligand_0, protein_0, protein_1, protein_2), 11 * radian,
-             12 * kcal_per_mol / (radian * radian)),
-        ]}
+    restraint_dict = {
+        "anchor_points":{"r1":protein_0, "r2":protein_1, "r3":protein_2,
+                         "l1":ligand_0, "l2":ligand_1, "l3":ligand_2},
+        "equilibrium_values":{"r0":7.84 * angstrom,
+                              "thetaA0":0.81 * radian,
+                              "thetaB0":1.74 * radian,
+                              "phiA0":2.59 * radian,
+                              "phiB0":-1.20 * radian,
+                              "phiC0":2.63 * radian},
+        "force_constants":{"kr":10 * kcal_per_mol / angstrom ** 2,
+                           "kthetaA":10 * kcal_per_mol / (radian * radian),
+                           "kthetaB":10 * kcal_per_mol / (radian * radian),
+                           "kphiA":10 * kcal_per_mol / (radian * radian),
+                           "kphiB":10 * kcal_per_mol / (radian * radian),
+                           "kphiC":10 * kcal_per_mol / (radian * radian)}}
+    restraint = Restraint(system, restraint_dict, type='Boresch')
     return restraint
 
-class TestBoreschType():
-    '''Test the output type of the Boresch.'''
+def test_sanity(restraint):
+    'Sanity check'
+    assert isinstance(restraint, Restraint)
+
+class TestGromacsOutput():
     @staticmethod
     @pytest.fixture(scope='class')
-    def Boresch(restraint):
-        return restraint._Boresch
+    def Topology(restraint):
+        return restraint.toString(engine='Gromacs').split('\n')
 
-    @pytest.mark.parametrize("name", ['Bonds', 'Angles', 'Dihedrals'])
-    def test_type_atom(self, Boresch, name):
-        'Check if the atoms are the type atom'
-        fields = Boresch[name]
-        for field in fields:
-            for atom in field[0]:
-                assert isinstance(atom, Atom)
+    def test_sanity(self, Topology):
+        'Sanity check'
+        assert 'intermolecular_interactions' in Topology[0]
 
-    @pytest.mark.parametrize("name,length", [
-        ('Bonds', 2), ('Angles', 3), ('Dihedrals', 4)])
-    def test_length_atom(self, Boresch, name, length):
-        'Check if the bond, angle, dihedral have the correct number of atoms.'
-        fields = Boresch[name]
-        for field in fields:
-            assert len(field[0]) == length
+    def test_bond(self, Topology):
+        ai, aj, type, bA, kA, bB, kB = Topology[3].split()
+        assert ai == '1'
+        assert aj == '1496'
+        assert bA == '0.784'
+        assert bB == '0.784'
+        assert kB == '4184.00'
 
-    @pytest.mark.parametrize("name,unit", [
-        ('Bonds', Length), ('Angles', Angle), ('Dihedrals', Angle)])
-    def test_unit_value(self, Boresch, name, unit):
-        '''Check if the the equilibrium value of bond, angle, dihedral has the
-        correct unit.'''
-        fields = Boresch[name]
-        for field in fields:
-            assert isinstance(field[1], unit)
+    def test_angle(self, Topology):
+        ai, aj, ak, type, thA, kA, thB, kB = Topology[6].split()
+        assert ai == '2'
+        assert aj == '1'
+        assert ak == '1496'
+        assert thA == '46.410'
+        assert thB == '46.410'
+        assert kB == '41.84'
+        ai, aj, ak, type, thA, kA, thB, kB = Topology[7].split()
+        assert ai == '1'
+        assert aj == '1496'
+        assert ak == '1497'
 
-    @pytest.mark.parametrize("name,angle,length", [
-        ('Bonds', 0, 0), ('Angles', -2, 2), ('Dihedrals', -2, 2)])
-    def test_unit_fc(self, Boresch, name, angle, length):
-        '''Check if the the force constant of bond, angle, dihedral has the
-        correct unit.'''
-        fields = Boresch[name]
-        for field in fields:
-            assert field[2].dimensions() == (angle, 0, length, 1, -1, 0, -2)
+    def test_dihedral(self, Topology):
+        ai, aj, ak, al, type, phiA, kA, phiB, kB = Topology[10].split()
+        assert ai == '3'
+        assert aj == '2'
+        assert ak == '1'
+        assert al == '1496'
+        assert phiA == '148.396'
+        assert phiB == '148.396'
+        assert kB == '41.84'
+        ai, aj, ak, al, type, phiA, kA, phiB, kB = Topology[11].split()
+        assert ai == '2'
+        assert aj == '1'
+        assert ak == '1496'
+        assert al == '1497'
+        ai, aj, ak, al, type, phiA, kA, phiB, kB = Topology[12].split()
+        assert ai == '1'
+        assert aj == '1496'
+        assert ak == '1497'
+        assert al == '1498'
