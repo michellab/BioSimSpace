2db1c048283ea766e20a07ca14c22f231636a518
finlayclark <s1629668@ed.ac.uk>
2022-08-04 10:41:42 +0100

    Test SOMD abfe

--- test/Sandpit/Exscientia/Free_Energy/test_relative.py
+++ test/Sandpit/Exscientia/Free_Energy/test_relative.py
@@ -1,4 +1,5 @@
 import pathlib
+from os.path import exists
 
 import pytest
 import pandas as pd
@@ -10,7 +11,7 @@ except ModuleNotFoundError:
     is_alchemlyb = False
 
 import BioSimSpace.Sandpit.Exscientia as BSS
-from BioSimSpace.Sandpit.Exscientia.Protocol import FreeEnergyEquilibration
+from BioSimSpace.Sandpit.Exscientia.Protocol import FreeEnergyEquilibration, FreeEnergy
 from BioSimSpace.Sandpit.Exscientia.Align._decouple import decouple
 from BioSimSpace.Sandpit.Exscientia import Types as _Types
 
@@ -54,3 +55,40 @@ class Test_gmx_ABFE():
             assert u_nk.index.names == ['time', 'coul-lambda', 'vdw-lambda']
             assert np.isclose(u_nk.index.values[0][1], coul)
             assert np.isclose(u_nk.index.values[0][2], vdw)
+
+class Test_Somd_ABFE():
+    @staticmethod
+    @pytest.fixture(scope='class')
+    def freenrg():
+        m = BSS.Parameters.openff_unconstrained_2_0_0(
+                    "c1ccccc1").getMolecule()
+        mol = decouple(m)
+        system = mol.toSystem()
+
+        decouple_m = decouple(m)
+        solvated = BSS.Solvent.tip3p(molecule=decouple_m,
+                                     box=3 * [3 * BSS.Units.Length.nanometer])
+        protocol = BSS.Protocol.FreeEnergy(lam_vals = [0.0, 0.5, 1.0],
+                              runtime=0.0001*BSS.Units.Time.nanosecond)
+        freenrg = BSS.FreeEnergy.Relative(solvated, protocol,
+                                          engine='SOMD')
+
+        freenrg.run()
+        freenrg.wait()
+        return freenrg
+
+    def test_files_exist(self, freenrg):
+        '''Test if all the files have been created.'''
+        path = pathlib.Path(freenrg._work_dir)
+        # TODO: Fix _process_runner.wait so this works with range(3)
+        for i in range(1):
+            assert (path / f'lambda_{i}' / 'simfile.dat').is_file()
+            assert (path / f'lambda_{i}' / 'sim_restart.s3').is_file()
+            assert (path / f'lambda_{i}' / 'traj000000001.dcd').is_file()
+            assert (path / f'lambda_{i}' / 'gradients.dat').is_file()
+
+    def test_correct_simfile(self, freenrg):
+        '''Check if correct numbers of entries in simfile.dat'''
+        path = pathlib.Path(freenrg._work_dir)
+        with open(path / 'lambda_0' / 'simfile.dat', 'rt') as f:
+            assert "#Alchemical array is		 (0.0, 0.5, 1.0)" in f.read()
