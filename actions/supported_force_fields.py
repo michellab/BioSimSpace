69850d847eb56576c09704b75c9a46ab10fab9eb
Lester Hedges <lester.hedges@gmail.com>
2022-11-14 14:09:14 +0000

    Formatting tweaks. [ci skip]

--- actions/supported_force_fields.py
+++ actions/supported_force_fields.py
@@ -4,6 +4,6 @@
 # Import the name of all publicly exposed functions.
 from BioSimSpace.Parameters._parameters import __all__ as parameters
 
-# Sort and print separated by an literal newline character and four spaces.
+# Sort and print separated by a literal newline character and four spaces.
 parameters.sort()
 print("\\n    ".join(parameters))
