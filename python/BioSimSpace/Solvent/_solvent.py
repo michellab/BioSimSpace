34392b761c0c637c2a23be055d1b3485ff73ed1b
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2022-12-22 00:09:15 +0000

    Another Vector / units / need .value() fix

--- python/BioSimSpace/Solvent/_solvent.py
+++ python/BioSimSpace/Solvent/_solvent.py
@@ -653,7 +653,8 @@ def _solvate(molecule, box, angles, shell, model, num_point,
         box_center = triclinic_box.cellMatrix()*_Vector(0.5, 0.5, 0.5)
 
         # Work out the offset between the molecule and box centers.
-        shift = [_Length(box_center[x]-center[x], "Angstrom") for x in range(0, 3)]
+        shift = [_Length(box_center[x].value()-center[x].value(),
+                 "Angstrom") for x in range(0, 3)]
 
         # Center the solute in the box.
         molecule.translate(shift)
