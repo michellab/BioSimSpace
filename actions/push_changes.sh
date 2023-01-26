64a8ab396db69422464ead5b33df19df6f4ca592
Lester Hedges <lester.hedges@gmail.com>
2023-01-26 13:22:06 +0000

    Remove redundant website related actions scripts.

--- actions/push_changes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Get the website repository user token.
-WEBSITE_TOKEN=${WEBSITE_TOKEN:-$1}
-
-git push --repo https://biosimspacebot:$WEBSITE_TOKEN@github.com/michellab/BioSimSpaceWebsite.git > /dev/null 2>&1
