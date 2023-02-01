1e6782d3c7cfce0a14667cb728d4824feeb9d6da
Lester Hedges <lester.hedges@gmail.com>
2023-02-01 13:37:36 +0000

    Remove old docker directory. [ci skip]

--- docker/document-devel/supported_force_fields.py
+++
@@ -1,9 +0,0 @@
-# Utility script to print the names of all dynamically generated functions
-# within the BioSimSpace.Parameters._parameters module.
-
-# Import the name of all publicly exposed functions.
-from BioSimSpace.Parameters._parameters import __all__ as parameters
-
-# Sort and print separated by an literal newline character and four spaces.
-parameters.sort()
-print("\\n    ".join(parameters))
