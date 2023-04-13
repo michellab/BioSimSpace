cab16981f62f34d7548d841239b3bfaa95f8baba
annamherz <annaherz98@yahoo.com>
2023-04-13 15:31:32 +0100

    fix of ligand param w openff as in openbiosim BSS

--- python/BioSimSpace/Parameters/Protocol/_openforcefield.py
+++ python/BioSimSpace/Parameters/Protocol/_openforcefield.py
@@ -1,13 +1,13 @@
 ######################################################################
 # BioSimSpace: Making biomolecular simulation a breeze!
 #
-# Copyright: 2017-2022
+# Copyright: 2017-2023
 #
 # Authors: Lester Hedges <lester.hedges@gmail.com>
 #
 # BioSimSpace is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # BioSimSpace is distributed in the hope that it will be useful,
@@ -22,7 +22,7 @@
 """
 Functionality for handling parameterisation protocols
 for force fields from the Open Force Field Initiative.
-Author: Lester Hedges <lester.hedges@gmail.com>
+Author: Lester Hedges <lester.hedges@gmail.com>.
 """
 
 __author__ = "Lester Hedges"
@@ -36,19 +36,17 @@ __all__ = ["OpenForceField"]
 from ..._Utils import _try_import, _have_imported
 
 import os as _os
+
 _parmed = _try_import("parmed")
 import queue as _queue
 import subprocess as _subprocess
 
 import warnings as _warnings
-# Suppress numpy warnings from RDKit import.
-_warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-_warnings.filterwarnings("ignore", message="numpy.ndarray size changed")
-_warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 # Suppress duplicate to-Python converted warnings.
 # Both Sire and RDKit register the same converter.
 with _warnings.catch_warnings():
-    _warnings.filterwarnings("ignore")
+    _warnings.simplefilter("ignore")
     _rdkit = _try_import("rdkit")
 
     if _have_imported(_rdkit):
@@ -56,12 +54,13 @@ with _warnings.catch_warnings():
         from rdkit import RDLogger as _RDLogger
 
         # Disable RDKit warnings.
-        _RDLogger.DisableLog('rdApp.*')
+        _RDLogger.DisableLog("rdApp.*")
     else:
         _Chem = _rdkit
         _RDLogger = _rdkit
 
 import sys as _sys
+
 # Temporarily redirect stderr to suppress import warnings.
 _sys.stderr = open(_os.devnull, "w")
 
@@ -75,11 +74,12 @@ else:
 _openff = _try_import("openff")
 
 if _have_imported(_openff):
+    from openff.interchange import Interchange as _Interchange
     from openff.toolkit.topology import Molecule as _OpenFFMolecule
     from openff.toolkit.topology import Topology as _OpenFFTopology
-    from openff.toolkit.typing.engines.smirnoff import ForceField \
-        as _Forcefield
+    from openff.toolkit.typing.engines.smirnoff import ForceField as _Forcefield
 else:
+    _Interchange = _openff
     _OpenFFMolecule = _openff
     _OpenFFTopology = _openff
     _Forcefield = _openff
@@ -97,25 +97,28 @@ from ... import IO as _IO
 from ..._Exceptions import IncompatibleError as _IncompatibleError
 from ..._Exceptions import ThirdPartyError as _ThirdPartyError
 from ..._SireWrappers import Molecule as _Molecule
+from ... import _Utils
 
 from . import _protocol
+
 
 class OpenForceField(_protocol.Protocol):
     """A class for handling protocols for Open Force Field models."""
 
     def __init__(self, forcefield, property_map={}):
-        """Constructor.
+        """
+        Constructor.
 
-           Parameters
-           ----------
+        Parameters
+        ----------
 
-           forcefield : str
-               The name of the force field.
+        forcefield : str
+            The name of the force field.
 
-           property_map : dict
-               A dictionary that maps system "properties" to their user defined
-               values. This allows the user to refer to properties with their
-               own naming scheme, e.g. { "charge" : "my-charge" }
+        property_map : dict
+            A dictionary that maps system "properties" to their user defined
+            values. This allows the user to refer to properties with their
+            own naming scheme, e.g. { "charge" : "my-charge" }
         """
 
         # Call the base class constructor.
@@ -125,31 +128,36 @@ class OpenForceField(_protocol.Protocol):
         self._tleap = False
         self._pdb2gmx = False
 
+
+
     def run(self, molecule, work_dir=None, queue=None):
-        """Run the parameterisation protocol.
+        """
+        Run the parameterisation protocol.
 
-           Parameters
-           ----------
+        Parameters
+        ----------
 
-           molecule : :class:`Molecule <BioSimSpace._SireWrappers.Molecule>`, str
-               The molecule to parameterise, either as a Molecule object or SMILES
-               string.
+        molecule : :class:`Molecule <BioSimSpace._SireWrappers.Molecule>`, str
+            The molecule to parameterise, either as a Molecule object or SMILES
+            string.
 
-           work_dir : str
-               The working directory.
+        work_dir : str
+            The working directory.
 
-           queue : queue.Queue
-               The thread queue is which this method has been run.
+        queue : queue.Queue
+            The thread queue is which this method has been run.
 
-           Returns
-           -------
+        Returns
+        -------
 
-           molecule : BioSimSpace._SireWrappers.Molecule
-               The parameterised molecule.
+        molecule : BioSimSpace._SireWrappers.Molecule
+            The parameterised molecule.
         """
 
         if not isinstance(molecule, (_Molecule, str)):
-            raise TypeError("'molecule' must be of type 'BioSimSpace._SireWrappers.Molecule' or 'str'")
+            raise TypeError(
+                "'molecule' must be of type 'BioSimSpace._SireWrappers.Molecule' or 'str'"
+            )
 
         if work_dir is not None and not isinstance(work_dir, str):
             raise TypeError("'work_dir' must be of type 'str'")
@@ -200,25 +208,31 @@ class OpenForceField(_protocol.Protocol):
             # If the molecule was originally loaded from an SDF format file,
             # then write back to the same format.
             fileformat_prop = self._property_map.get("fileformat", "fileformat")
-            if (molecule._sire_object.hasProperty(fileformat_prop) and
-                "SDF" in molecule._sire_object.property("fileformat").value()):
-                    # Write the molecule to SDF format.
-                    try:
-                        _IO.saveMolecules(prefix + "molecule", molecule, "sdf")
-                    except Exception as e:
-                        msg = "Failed to write the molecule to 'SDF' format."
-                        if _isVerbose():
-                            msg += ": " + getattr(e, "message", repr(e))
-                            raise IOError(msg) from e
-                        else:
-                            raise IOError(msg) from None
+            if (
+                molecule._sire_object.hasProperty(fileformat_prop)
+                and "SDF" in molecule._sire_object.property("fileformat").value()
+            ):
+                # Write the molecule to SDF format.
+                try:
+                    _IO.saveMolecules(
+                        prefix + "molecule", molecule, "sdf", self._property_map
+                    )
+                except Exception as e:
+                    msg = "Failed to write the molecule to 'SDF' format."
+                    if _isVerbose():
+                        msg += ": " + getattr(e, "message", repr(e))
+                        raise IOError(msg) from e
+                    else:
+                        raise IOError(msg) from None
 
             # Otherwise, go via an intermediate PDB file and use RDKit to try
             # to recover stereochemistry.
             else:
                 # Write the molecule to a PDB file.
                 try:
-                    _IO.saveMolecules(prefix + "molecule", molecule, "pdb")
+                    _IO.saveMolecules(
+                        prefix + "molecule", molecule, "pdb", self._property_map
+                    )
                 except Exception as e:
                     msg = "Failed to write the molecule to 'PDB' format."
                     if _isVerbose():
@@ -229,7 +243,9 @@ class OpenForceField(_protocol.Protocol):
 
                 # Create an RDKit molecule from the PDB file.
                 try:
-                    rdmol = _Chem.MolFromPDBFile(prefix + "molecule.pdb", removeHs=False)
+                    rdmol = _Chem.MolFromPDBFile(
+                        prefix + "molecule.pdb", removeHs=False
+                    )
                 except Exception as e:
                     msg = "RDKit was unable to read the molecular PDB file!"
                     if _isVerbose():
@@ -286,33 +302,13 @@ class OpenForceField(_protocol.Protocol):
             else:
                 raise _ThirdPartyError(msg) from None
 
-        # Obtain the OpenMM Topology object from the OpenFF topology.
+        # Create an Interchange object.
         try:
-            omm_topology = off_topology.to_openmm()
+            interchange = _Interchange.from_smirnoff(
+                force_field=forcefield, topology=off_topology
+            )
         except Exception as e:
-            msg = "Unable to convert Open Force Field topology to OpenMM topology!"
-            if _isVerbose():
-                msg += ": " + getattr(e, "message", repr(e))
-                raise _ThirdPartyError(msg) from e
-            else:
-                raise _ThirdPartyError(msg) from None
-
-        # Create an OpenMM system.
-        try:
-            omm_system = forcefield.create_openmm_system(off_topology)
-        except Exception as e:
-            msg = "Unable to create OpenMM System!"
-            if _isVerbose():
-                msg += ": " + getattr(e, "message", repr(e))
-                raise _ThirdPartyError(msg) from e
-            else:
-                raise _ThirdPartyError(msg) from None
-
-        # Convert the OpenMM System to a ParmEd structure.
-        try:
-            parmed_structure = _parmed.openmm.load_topology(omm_topology, omm_system, off_molecule.conformers[0])
-        except Exception as e:
-            msg = "Unable to convert OpenMM System to ParmEd structure!"
+            msg = "Unable to create OpenFF Interchange object!"
             if _isVerbose():
                 msg += ": " + getattr(e, "message", repr(e))
                 raise _ThirdPartyError(msg) from e
@@ -321,10 +317,10 @@ class OpenForceField(_protocol.Protocol):
 
         # Export AMBER format files.
         try:
-            parmed_structure.save(prefix + "parmed.prmtop", overwrite=True)
-            parmed_structure.save(prefix + "parmed.inpcrd", overwrite=True)
+            interchange.to_prmtop(prefix + "interchange.prmtop")
+            interchange.to_inpcrd(prefix + "interchange.inpcrd")
         except Exception as e:
-            msg = "Unable to write ParmEd structure to AMBER format!"
+            msg = "Unable to write Interchange object to AMBER format!"
             if _isVerbose():
                 msg += ": " + getattr(e, "message", repr(e))
                 raise _ThirdPartyError(msg) from e
@@ -333,7 +329,9 @@ class OpenForceField(_protocol.Protocol):
 
         # Load the parameterised molecule. (This could be a system of molecules.)
         try:
-            par_mol = _IO.readMolecules([prefix + "parmed.prmtop", prefix + "parmed.inpcrd"])
+            par_mol = _IO.readMolecules(
+                [prefix + "interchange.prmtop", prefix + "interchange.inpcrd"]
+            )
             # Extract single molecules.
             if par_mol.nMolecules() == 1:
                 par_mol = par_mol.getMolecules()[0]
@@ -353,8 +351,15 @@ class OpenForceField(_protocol.Protocol):
             # this will be missing.
 
             # Rename the molecule with the original SMILES string.
+            # Since the name is written to topology file formats, we
+            # need to ensure that it doesn't start with an [ character,
+            # which would break GROMACS.
+            name = molecule
+            if name.startswith("["):
+                name = f"smiles:{name}"
+
             edit_mol = new_mol._sire_object.edit()
-            edit_mol = edit_mol.rename(molecule).molecule()
+            edit_mol = edit_mol.rename(name).molecule()
 
             # Rename the residue LIG.
             resname = _SireMol.ResName("LIG")
@@ -365,7 +370,9 @@ class OpenForceField(_protocol.Protocol):
 
         else:
             new_mol = molecule.copy()
-            new_mol.makeCompatibleWith(par_mol, property_map=self._property_map, overwrite=True, verbose=False)
+            new_mol.makeCompatibleWith(
+                par_mol, property_map=self._property_map, overwrite=True, verbose=False
+            )
 
         # Record the forcefield used to parameterise the molecule.
         new_mol._forcefield = self._forcefield
