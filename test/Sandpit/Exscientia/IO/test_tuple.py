f04767333305aa57ff411669d090eaac683bf839
Lester Hedges <lester.hedges@gmail.com>
2023-04-18 09:32:30 +0100

    Blacken.

--- test/Sandpit/Exscientia/IO/test_tuple.py
+++ test/Sandpit/Exscientia/IO/test_tuple.py
@@ -1,5 +1,6 @@
 import BioSimSpace.Sandpit.Exscientia as BSS
 
+
 def test_tuple():
     """Check that we can read from a tuple of files."""
     BSS.IO.readMolecules(("test/input/ala.crd", "test/input/ala.top"))
